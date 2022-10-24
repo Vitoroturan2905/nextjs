0d8823f7a80a995599419a8dc2dfb269af50ccfb
Vitor Lima Silva <110677656+Vitoroturan2905@users.noreply.github.com>
2022-10-24 09:47:11 -0300

    Add files via upload
    commit teste

---
+++ src/componentes/storys/Perfis.js
@@ -0,0 +1,16 @@
+import img1 from './imagens/t.png';
+
+function Perfis (props){
+    return(
+        <div className="perfis">
+            <img className='img_perfil' src={img1} alt="Logo do instagram"  />
+            <a className="a_foto">Thainara Costa</a>
+            </div>
+
+
+
+       
+            
+    )
+}
+export default Perfis;
